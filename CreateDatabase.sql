0faed429ac6c4687978438cfdce08ca8aa9087d1
Alan <lialan478@yahoo.com>
2022-09-01 11:54:30 -0700

    Added description to readme

---
+++ CreateDatabase.sql
@@ -0,0 +1,12 @@
+create table animals (idno int, name varchar(255), origin varchar(255), zookeeper varchar(255),PRIMARY KEY (idno));
+
+insert into animals values(1,'Lion','Africa','Bob');
+insert into animals values(2,'Cheetah','Africa','Bob');
+insert into animals values(3,'Elephant','Africa','Rick');
+insert into animals values(4,'Zebra','Africa','Rick');
+insert into animals values(5,'Hippo','Africa','Rick');
+insert into animals values(6,'Penguin','Antarctica','Zack');
+insert into animals values(7,'Whale','Antarctica','Zack');
+insert into animals values(8,'Panda','Asia','Jess');
+insert into animals values(9,'Pangolin','Asia','Jess');
+insert into animals values(10,'Tiger','Asia','Peter');
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Zoo
+This project represents a zoo.
+The database is comprised of a list of animals
+followed by their place of origin and the zookeeper
+who takes care of them. 
+
+You should be able to do CRUD operations with the database.
+Available operations will depend on the level of the user.
