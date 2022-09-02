321360242076a09f6e5ce4e74856fad9e1078999
Alejandro Garcia <alegarcia@Alejandros-MBP.attlocal.net>
2022-09-02 11:04:32 -0700

    tweaked animal, added assignment and zookeeper

---
+++ zoo/src/main/java/com/example/models/Zookeeper.java
@@ -0,0 +1,37 @@
+package com.example.models;
+
+import javax.persistence.*;
+
+@Table()
+@Entity
+public class Zookeeper {
+    @Id
+    int empid;
+    String name;
+    int salary;
+    public Zookeeper(int empid, String name, int salary) {
+        this.empid = empid;
+        this.name = name;
+        this.salary = salary;
+    }
+    public Zookeeper() {
+    }
+    public int getEmpid() {
+        return empid;
+    }
+    public void setEmpid(int empid) {
+        this.empid = empid;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getSalary() {
+        return salary;
+    }
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
